f56a3c2b4cf1737568e9833df79e15981b0b2ca7
Bhavesh <bhavesh189kumar@gmail.com>
2021-10-21 20:17:52 +0530

    Data cleaning done using nltk and porter stemmer.

--- Code/pre-processing/data_cleaning.py
+++ Code/pre-processing/data_cleaning.py
@@ -1,11 +1,23 @@
-import texthero as hero
+import re
+import nltk
+from nltk import data
 import numpy as np
 import pandas as pd
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 
 dataset = pd.read_csv("../../Datasets/JobsIT_Dataset.csv")
 dataset = dataset.loc[:, ["Query", "Description"]]
 
-dataset['Description'] = hero.clean(dataset["Description"])
-dataset['Description'] = hero.preprocessing.stem(dataset["Description"])
+for i in range(len(dataset)):
+    description = re.sub('[^a-zA-Z]', ' ', dataset["Description"][i])
+    description = description.lower()
+    description = description.split()
+    ps = PorterStemmer()
+    all_stopwords = stopwords.words('english')
+    description = [ps.stem(word) for word in description if not word in set(all_stopwords)]
+    description = ' '.join(description)
+    dataset["Description"][i] = description
 
 dataset.to_csv("../../Cleaned_Datasets/JobsIT_Dataset.csv", index=False)
