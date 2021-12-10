94ab3e4b5e14330f86df8b5767a58e9e6b2d2606
Bhavesh <bhaveshkumar.c02@gmail.com>
2021-12-10 15:53:26 +0530

    Interactive program for model.py

--- Code/model.py
+++ Code/model.py
@@ -1,4 +1,5 @@
 import re
+import sys, getopt
 import pandas as pd
 import numpy as np
 from nltk.corpus import stopwords
@@ -6,12 +7,32 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.stem.porter import PorterStemmer
 
+argumentList = sys.argv[1:]
+options = "o:f:"
+long_options = ["option", "filename"]
+arguments, values = getopt.getopt(argumentList, options, long_options)
+
+filename, option = "input.txt", "IT"
+
+for argument, value in arguments:
+    if argument in ["-f", "--filename"]:
+        filename = value
+    elif argument in ["-o", "--option"]:
+        option = value
+
+if option == "IT":
+    path = "../Cleaned_Datasets/JobsIT_Dataset.csv"
+elif option == "NON-IT":
+    path = "../Cleaned_Datasets/JobsNonIT_Dataset.csv"
+else:
+    print("Invalid value for argument option")
+    sys.exit(1)
 
 all_stopwords = stopwords.words('english')
 
 tf = TfidfVectorizer()
-jobs = pd.read_csv("../Cleaned_Datasets/JobsIT_Dataset.csv")
-with open("../predictor_files/input.txt",encoding="utf8") as f:
+jobs = pd.read_csv(path)
+with open(f"../predictor_files/{filename}",encoding="utf8") as f:
     prediction_text = f.readlines()
 
 prediction_text = ' '.join(prediction_text)
@@ -32,10 +53,10 @@ for i in range(len(similarity_measure)):
     similarity_scores[jobs["Query"][i]]["sum"] += similarity_measure[i][0]
     similarity_scores[jobs["Query"][i]]["count"] += 1
 
-prediction, maximum = "", -1
+predictions = []
 for label in similarity_scores:
     avg = similarity_scores[label]["sum"]/similarity_scores[label]["count"]
-    if avg>maximum:
-        maximum = avg
-        prediction = label
-print(prediction, similarity_scores[prediction])
+    predictions.append([avg, label])
+predictions.sort(key = lambda key: -key[0])
+output_text = f"Top3 predictions for you in {option} Industry:\n" + '\n'.join(x[1] for x in predictions[:3])
+print(output_text)
