e95268e0f55290cd57603699d13bb2781e07efb6
Bhavesh <bhaveshkumar.c02@gmail.com>
2021-12-10 15:24:47 +0530

    print statements in data cleaning to inform the status.

--- Code/data_cleaning.py
+++ Code/data_cleaning.py
@@ -7,10 +7,14 @@ nltk.download("stopwords")
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+print("Starting preprocessing phase ...")
+
 dataset_IT = pd.read_csv("../Datasets/JobsIT_Dataset.csv")
 dataset_IT = dataset_IT.loc[:, ["Query", "Description"]]
 
 all_stopwords = stopwords.words('english')
+
+print("Processing IT dataset ...")
 
 for i in range(len(dataset_IT)):
     description = re.sub('[^a-zA-Z]', ' ', dataset_IT["Description"][i])
@@ -23,8 +27,12 @@ for i in range(len(dataset_IT)):
 
 dataset_IT.to_csv("../Cleaned_Datasets/JobsIT_Dataset.csv", index=False)
 
+print("IT dataset successfully cleaned! ..")
+
 dataset_nonIT = pd.read_csv("../Datasets/JobsNonIT_Dataset.csv")
 dataset_nonIT = dataset_nonIT.loc[:, ["Query", "Description"]]
+
+print("Processing NONIT Dataset ...")
 
 for i in range(len(dataset_nonIT)):
     description = re.sub('[^a-zA-Z]', ' ', dataset_nonIT['Description'][i])
@@ -36,3 +44,7 @@ for i in range(len(dataset_nonIT)):
     dataset_nonIT["Description"][i] = description
     
 dataset_nonIT.to_csv("../Cleaned_Datasets/JobsNonIT_Dataset.csv",index=False)
+
+print("NONIT dataset successfully cleaned! ..")
+
+print("Preprocessing phase finished!")
