4381a7cabe6e46934e03b982b2fbf2ea42d9169d
Bhavesh <bhaveshkumar.c02@gmail.com>
2021-12-10 17:02:06 +0530

    implemented cosine similarity

--- Code/model.py
+++ Code/model.py
@@ -6,6 +6,7 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.stem.porter import PorterStemmer
+from cosine_similarity import cosine_similarity as cs
 
 argumentList = sys.argv[1:]
 options = "o:f:"
